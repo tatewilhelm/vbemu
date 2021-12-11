f606504f8b71d585dbaff80caba2dfd570cc15b1
Tate Wilhelm <t8wilhelm@gmail.com>
2021-12-10 22:23:27 -0600

    made base main files and setup cmake

---
+++ src/main.c
@@ -0,0 +1,11 @@
+//
+// Created by averagedolphin on 12/10/21.
+//
+
+#include "main.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("l");
+}
---
+++ src/main.h
@@ -0,0 +1,9 @@
+//
+// Created by averagedolphin on 12/10/21.
+//
+
+#ifndef VBEMU_MAIN_H
+#define VBEMU_MAIN_H
+
+
+#endif //VBEMU_MAIN_H
