1b3b8e980df023212c5721456ca4b0e0a9d55a40
Tate Wilhelm <t8wilhelm@gmail.com>
2021-12-14 09:40:14 -0600

    Removed references to gameboy in other branch

--- src/gb/cpu.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by averagedolphin on 12/12/21.
-//
-
-#include "cpu.h"
--- src/gb/cpu.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by averagedolphin on 12/12/21.
-//
-
-#ifndef VBEMU_CPU_H
-#define VBEMU_CPU_H
-
-#endif //VBEMU_CPU_H
