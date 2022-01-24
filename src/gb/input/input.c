7cb4d3fd1edc62d91a83134e95d75bbf0d1d7a5a
Tate Wilhelm <t8wilhelm@gmail.com>
2022-01-24 14:35:14 -0600

    Made Input for dpad and buttons n stuff

---
+++ src/gb/input/input.c
@@ -0,0 +1,5 @@
+//
+// Created by averagedolphin on 1/24/22.
+//
+
+#include "input.h"
---
+++ src/gb/input/input.h
@@ -0,0 +1,8 @@
+//
+// Created by averagedolphin on 1/24/22.
+//
+
+#ifndef VBEMU_INPUT_H
+#define VBEMU_INPUT_H
+
+#endif //VBEMU_INPUT_H
